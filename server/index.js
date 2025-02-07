d3a5c0ac685f5f32e2188ae77c7e67dbb3773ac7
Bernard <benbaah104@gmail.com>
2025-02-07 02:58:41 +0000

    public Address recovery

--- server/index.js
+++ server/index.js
@@ -7,30 +7,32 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-	"02d4b806ba28a3c262e1bdfe270c1fb529fe1105eed396251550bd6a56fd8e5b26": 100,
-	"03e4174423336d74b3fbe85023c89487e7e354637abeb6d4a2a2fba5ebd151f6fd": 50,
-	"0393ebf162c8ff49ad93de9d9acc4695d9a471f04385dad17321a07d0b9f5dd43f": 75,
+	"02bf78871e24d8588235731d50fecc88a2f7639de2ee5a994bf4972f1c03f32ad7": 100,
+	"03515971dacf1d0b3c513c70bf7d007bc494789716a9d4a78edc59db04cbe85a77": 50,
+	"03d49dc812131de8acb6e958a552d2d97a70fe96d0627b8f759d226e3443ca43ba": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+	const { address } = req.params;
+	const balance = balances[address] || 0;
+	res.send({ balance });
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+	//TODO: GET SIGNATURE FROM CLIENT APP
+	// RECOVER THE PUBLIC ADDRESS FROM THE SIGNATURE
+	const { sender, recipient, amount } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+	setInitialBalance(sender);
+	setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
+	if (balances[sender] < amount) {
+		res.status(400).send({ message: "Not enough funds!" });
+	} else {
+		balances[sender] -= amount;
+		balances[recipient] += amount;
+		res.send({ balance: balances[sender] });
+	}
 });
 
 app.listen(port, () => {
